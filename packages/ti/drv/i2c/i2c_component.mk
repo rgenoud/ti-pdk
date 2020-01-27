0e2538c756926c961a6cddbe5a2ff4ac3ba62e6c
Hao Zhang <hzhang@ti.com>
2020-01-27 10:40:00 -0500

    board: am64x: add I2C lib support
    Signed-off-by: Hao Zhang <hzhang@ti.com>

--- packages/ti/drv/i2c/i2c_component.mk
+++ packages/ti/drv/i2c/i2c_component.mk
@@ -66,10 +66,10 @@
 #
 ifeq ($(i2c_component_make_include), )
 
-drvi2c_BOARDLIST       = am65xx_evm am65xx_idk j721e_sim j721e_evm
-drvi2c_BOARDLISTLIM    = am65xx_evm am65xx_idk
-drvi2c_SOCLIST         = am574x am572x am571x tda2xx tda2px tda2ex tda3xx dra78x dra72x dra75x k2h k2k k2l k2e k2g c6678 c6657 am437x am335x omapl137 omapl138 am65xx j721e j7200
-drvi2c_SOCLISTLIM      = am574x am572x am571x tda2xx tda2px tda2ex tda3xx dra78x dra72x dra75x k2h k2k k2l k2e k2g c6678 c6657 am437x am335x omapl137 omapl138 am65xx
+drvi2c_BOARDLIST       = am65xx_evm am65xx_idk j721e_sim j721e_evm am64x_evm
+drvi2c_BOARDLISTLIM    = am65xx_evm am65xx_idk am64x_evm
+drvi2c_SOCLIST         = am574x am572x am571x tda2xx tda2px tda2ex tda3xx dra78x dra72x dra75x k2h k2k k2l k2e k2g c6678 c6657 am437x am335x omapl137 omapl138 am65xx j721e j7200 am64x
+drvi2c_SOCLISTLIM      = am574x am572x am571x tda2xx tda2px tda2ex tda3xx dra78x dra72x dra75x k2h k2k k2l k2e k2g c6678 c6657 am437x am335x omapl137 omapl138 am65xx am64x
 drvi2c_tda2xx_CORELIST = ipu1_0
 drvi2c_tda2px_CORELIST = ipu1_0
 drvi2c_tda2ex_CORELIST = ipu1_0
@@ -96,6 +96,8 @@ drvi2c_j721e_CORELIST   = $(DEFAULT_j721e_CORELIST)
 drvi2c_j721e_CORELISTARM = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1
 drvi2c_j7200_CORELIST   = $(DEFAULT_j7200_CORELIST)
 drvi2c_j7200_CORELISTARM = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1
+drvi2c_am64x_CORELIST = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 ipu1_0
+
 ############################
 # i2c package
 # List of components included under i2c lib
@@ -358,7 +360,7 @@ export I2C_Baremetal_Eeprom_TestApp_CORE_DEPENDENCY
 export I2C_Baremetal_Eeprom_TestApp_MAKEFILE
 I2C_Baremetal_Eeprom_TestApp_PKG_LIST = I2C_Baremetal_Eeprom_TestApp
 I2C_Baremetal_Eeprom_TestApp_INCLUDE = $(I2C_Baremetal_Eeprom_TestApp_PATH)
-I2C_Baremetal_Eeprom_TestApp_BOARDLIST = am65xx_evm am65xx_idk j721e_evm
+I2C_Baremetal_Eeprom_TestApp_BOARDLIST = am65xx_evm am65xx_idk j721e_evm am64x_evm
 export I2C_Baremetal_Eeprom_TestApp_BOARDLIST
 ifeq ($(SOC),$(filter $(SOC), j721e))
 I2C_Baremetal_Eeprom_TestApp_$(SOC)_CORELIST = $(drvi2c_j721e_CORELISTARM)
