4b91c91eeaabb640cc93de66bbaa9359e9227979
Sheng Zhao <shengzhao@ti.com>
2020-08-24 23:34:15 -0500

    example: rtos_app: mimic udma_memcpy_testapp
    Signed-off-by: Sheng Zhao <shengzhao@ti.com>

--- packages/ti/drv/sciclient/examples/sciclient_rtos_app/sciclient_rtos_main.c
+++ packages/ti/drv/sciclient/examples/sciclient_rtos_app/sciclient_rtos_main.c
@@ -69,6 +69,8 @@
 #include <ti/sysbios/family/c7x/Mmu.h>
 #endif
 
+#include <ti/board/board.h>
+
 /* ========================================================================== */
 /*                           Macros & Typedefs                                */
 /* ========================================================================== */
@@ -137,21 +139,159 @@ int main(void)
     return retVal;
 }
 
+int32_t PsilPair()
+{
+    int32_t status = CSL_PASS;
+
+    struct tisci_msg_rm_psil_pair_req request;
+    request.nav_id = 199; //J721E_DEV_NAVSS0,
+    request.src_thread = 0x1040;
+    request.dst_thread = 0x1041 + 0x8000;
+    const Sciclient_ReqPrm_t      reqPrm =
+    {
+        TISCI_MSG_RM_PSIL_PAIR,
+        TISCI_MSG_FLAG_AOP,
+        (uint8_t *) &request,
+        sizeof(request),
+        SCICLIENT_SERVICE_WAIT_FOREVER
+    };
+
+    struct tisci_msg_rm_psil_pair_resp response;
+    Sciclient_RespPrm_t           respPrm =
+    {
+        0,
+        (uint8_t *) &response,
+        sizeof (response)
+    };
+
+    status = Sciclient_service(&reqPrm, &respPrm);
+    UART_printf("PsilPair: done. status =%d\n", status);
+
+    return status;
+}
+
+int32_t PsilUnpair()
+{
+    int32_t status = CSL_PASS;
+
+    struct tisci_msg_rm_psil_unpair_req request;
+    request.nav_id = 199; //J721E_DEV_NAVSS0,
+    request.src_thread = 0x1040;
+    request.dst_thread = 0x1041 + 0x8000;
+    const Sciclient_ReqPrm_t      reqPrm =
+    {
+        TISCI_MSG_RM_PSIL_UNPAIR,
+        TISCI_MSG_FLAG_AOP,
+        (uint8_t *) &request,
+        sizeof(request),
+        SCICLIENT_SERVICE_WAIT_FOREVER
+    };
+
+    struct tisci_msg_rm_psil_unpair_resp response;
+    Sciclient_RespPrm_t           respPrm =
+    {
+        0,
+        (uint8_t *) &response,
+        sizeof (response)
+    };
+
+    status = Sciclient_service(&reqPrm, &respPrm);
+    UART_printf("PsilUnpair: done. status =%d\n", status);
+
+    return status;
+}
+
+int32_t RmProxyCfg()
+{
+    int32_t status = CSL_PASS;
+
+    struct tisci_msg_rm_proxy_cfg_req request;
+    // request.nav_id = 234; //J721E_DEV_MCU_NAVSS0_PROXY0
+    request.nav_id = 210; //J721E_DEV_NAVSS0_PROXY_0
+    request.index = 36;
+    const Sciclient_ReqPrm_t      reqPrm =
+    {
+        TISCI_MSG_RM_PROXY_CFG,
+        TISCI_MSG_FLAG_AOP,
+        (uint8_t *) &request,
+        sizeof(request),
+        SCICLIENT_SERVICE_WAIT_FOREVER
+    };
+
+    struct tisci_msg_rm_proxy_cfg_resp response;
+    Sciclient_RespPrm_t           respPrm =
+    {
+        0,
+        (uint8_t *) &response,
+        sizeof (response)
+    };
+
+    status = Sciclient_service(&reqPrm, &respPrm);
+    UART_printf("RmProxyCfg: done. status =%d\n", status);
+
+    return status;
+}
+
+int32_t RmGetResourceRange(
+    uint16_t            type,
+    uint8_t            subtype,
+    uint8_t            secondary_host)
+{
+    int32_t status = CSL_PASS;
+
+    struct tisci_msg_rm_get_resource_range_req request;
+    request.type = type;
+    request.subtype = subtype;
+    request.secondary_host = secondary_host;
+    const Sciclient_ReqPrm_t      reqPrm =
+    {
+        TISCI_MSG_RM_GET_RESOURCE_RANGE,
+        TISCI_MSG_FLAG_AOP,
+        (uint8_t *) &request,
+        sizeof(request),
+        SCICLIENT_SERVICE_WAIT_FOREVER
+    };
+
+    struct tisci_msg_rm_get_resource_range_resp response;
+    Sciclient_RespPrm_t           respPrm =
+    {
+        0,
+        (uint8_t *) &response,
+        sizeof (response)
+    };
+
+    status = Sciclient_service(&reqPrm, &respPrm);
+    UART_printf("RmGetResourceRange: done. status =%d\n", status);
+
+    return status;
+}
+
 void GetClkFreq(UArg arg0, UArg arg1)
 {
     int32_t status = CSL_PASS;
     volatile uint32_t loopForever = 1U;
 
+    Board_initCfg           boardCfg;
+
+    Sciclient_ConfigPrms_t config;
+    Sciclient_configPrmsInit(&config);
+    config.isSecureMode = 1;
+    status = Sciclient_init(&config);
+
+    boardCfg = BOARD_INIT_PINMUX_CONFIG |
+               BOARD_INIT_UART_STDIO;
+    Board_init(boardCfg);
+
     uint32_t modId = 83; //J721E_DEV_EHRPWM0;
     uint8_t clkId = 0; //J721E_DEV_EHRPWM0_VBUSP_CLK;
     uint32_t freqHz;
 
-    Sciclient_ConfigPrms_t        config =
-    {
-        SCICLIENT_SERVICE_OPERATION_MODE_POLLED,
-        NULL,
-        1
-    };
+    // Sciclient_ConfigPrms_t        config =
+    // {
+    //     SCICLIENT_SERVICE_OPERATION_MODE_POLLED,
+    //     NULL,
+    //     1
+    // };
 
     struct tisci_msg_get_freq_req request;
     request.device = (uint32_t) modId;
@@ -173,18 +313,27 @@ void GetClkFreq(UArg arg0, UArg arg1)
         sizeof (response)
     };
 
-    status = Sciclient_init(&config);
+    // status = Sciclient_init(&config);
     if (status == CSL_PASS)
     {
-        App_sciclientConsoleInit();
+        // App_sciclientConsoleInit();
+        UART_printf("RTOS application2\n");
+
+        status = RmProxyCfg();
+        RmGetResourceRange(213, 0, 36);
+        RmGetResourceRange(213, 0, 128);
+
+        status = PsilPair();
+        status = PsilUnpair();
+
+        request.device = 264; //J721E_DEV_SA2_UL0
+        request.clk = 0; //J721E_DEV_SA2_UL0_X2_CLK
+
         App_sciclientPrintf("SCIClient GetClkFreq\n");
 
         status = Sciclient_service(&reqPrm, &respPrm);
         freqHz = (uint32_t) (response.freq_hz & 0xFFFFFFFF);
         UART_printf("Freq = %d MHz\n", freqHz / 1000000);
-
-        request.device = 264; //J721E_DEV_SA2_UL0
-        request.clk = 0; //J721E_DEV_SA2_UL0_X2_CLK
 
         status = Sciclient_service(&reqPrm, &respPrm);
         freqHz = (uint32_t) (response.freq_hz & 0xFFFFFFFF);
