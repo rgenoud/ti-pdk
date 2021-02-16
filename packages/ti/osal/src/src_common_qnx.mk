d2a050c705bdd926c74e27d96687da2f2286b4ee
Praveen Rao <prao@ti.com>
2021-02-16 17:58:26 -0600

    [QNX] Osal: Update for QNX
    Signed-off-by: Praveen Rao <prao@ti.com>

---
+++ packages/ti/osal/src/src_common_qnx.mk
@@ -0,0 +1,21 @@
+
+SRCDIR = . src/qnx
+INCDIR = . src/qnx
+
+SRCS_COMMON += HwiP_qnx.c SwiP_qnx.c Utils_qnx.c SemaphoreP_qnx.c RegisterIntr_qnx.c \
+               TaskP_qnx.c Queue_qnx.c Delay_qnx.c CacheP_qnx.c
+
+ifeq ($(SOC),$(filter $(SOC), am65xx j721e j7200))
+  SRCDIR      += soc/$(SOC)
+  SRCS_COMMON +=
+endif
+
+ifeq ($(CORE),mpu1_0)
+  SRCDIR += arch/core/a53
+  SRCS_COMMON +=
+endif
+
+PACKAGE_SRCS_COMMON = makefile HwiP.h SwiP.h MuxIntcP.h osal.h osal_component.mk SemaphoreP.h CacheP.h \
+                      TimerP.h RegisterIntr.h EventCombinerP.h Queue.h TaskP.h EventP.h \
+                      arch soc src/qnx src/src_common_qnx.mk \
+                      build/makefile_qnx.mk
