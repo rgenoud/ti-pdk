be6747e67aa95a5174d74a012a8a6ac554dd88fd
repo/manifests/am62x/sync_psdk.sh <sebin.francis@ti.com>
2021-06-11 22:35:38 +0530

    AM62: Adding dummy board init fucntion for am62x_evm
    Board_init is used in OSAL unit test applciations, for that a dummy board
    init function is created.
    
    Signed-off-by: sebin <sebin.francis@ti.com>

---
+++ packages/ti/board/src/am62x_evm/src_files_am62x_evm.mk
@@ -0,0 +1,8 @@
+
+SRCDIR += src/am62x_evm src/am62x_evm/include
+INCDIR += src/am62x_evm src/am62x_evm/include
+
+# Common source files across all platforms and cores
+SRCS_COMMON += board_init.c 
+
+PACKAGE_SRCS_COMMON = src/am62x_evm/src_files_am62x_evm.mk
