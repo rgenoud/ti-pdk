30f9bc956ac4a703362dafb8f63b16547a3d6b54
Vishal Mahaveer <vishalm@ti.com>
2021-09-10 22:07:34 -0500

    sysfw_migrate.sh: update am62x variants for firmware header generation
    We have a VLAB and ZeBu config, update to generate headers and signed binary
    for these variants.
    
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- packages/ti/drv/sciclient/tools/sysfw_migrate.sh
+++ packages/ti/drv/sciclient/tools/sysfw_migrate.sh
@@ -268,7 +268,8 @@ if [ "$SKIP_GEN_BIN" != "YES" ];  then
     ./firmwareHeaderGen.sh j721s2-hs
     ./firmwareHeaderGen.sh j784s4-zebu
     # ./firmwareHeaderGen.sh j721s2-zebu
-    ./firmwareHeaderGen.sh am62x
+    ./firmwareHeaderGen.sh am62x-vlab
+    ./firmwareHeaderGen.sh am62x-zebu
 
 fi
 
