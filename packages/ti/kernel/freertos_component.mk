20d03a54b98afcafde90ee5767fbd1488ccadccc
Don Dominic <a0486429@ti.com>
2021-09-15 17:22:47 +0530

    J721S2: FreeRTOS: R5F: Enable freertos lib
    - Added config/j721s2
    - Makefile updates
    
    Signed-off-by: Don Dominic <a0486429@ti.com>

--- packages/ti/kernel/freertos_component.mk
+++ packages/ti/kernel/freertos_component.mk
@@ -66,13 +66,14 @@
 #
 ifeq ($(freertos_component_make_include), )
 
-freertos_BOARDLIST       = tpr12_evm j721e_evm j7200_evm am65xx_evm am65xx_idk awr294x_evm
-freertos_SOCLIST         = tpr12 j721e j7200 am65xx awr294x
+freertos_BOARDLIST       = tpr12_evm j721e_evm j7200_evm am65xx_evm am65xx_idk awr294x_evm j721s2_evm
+freertos_SOCLIST         = tpr12 j721e j7200 am65xx awr294x j721s2
 freertos_tpr12_CORELIST   = c66xdsp_1 mcu1_0 mcu1_1
 freertos_awr294x_CORELIST   = c66xdsp_1 mcu1_0 mcu1_1
 freertos_j721e_CORELIST   = mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1 c66xdsp_1 c66xdsp_2
 freertos_j7200_CORELIST   = mcu1_0 mcu1_1 mcu2_0 mcu2_1
 freertos_am65xx_CORELIST   = mcu1_0 mcu1_1
+freertos_j721s2_CORELIST   = mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1
 
 ############################
 # freertos package
