7875cd37e73509e5045dde1a09ba58dbd20cfef6
Ankur <a0132173@ti.com>
2021-09-16 15:24:03 +0530

    J721S2 Build support for i2c library
    Signed-off-by: Ankur <a0132173@ti.com>

--- packages/ti/drv/i2c/i2c_component.mk
+++ packages/ti/drv/i2c/i2c_component.mk
@@ -68,7 +68,7 @@ ifeq ($(i2c_component_make_include), )
 
 drvi2c_BOARDLIST       = am65xx_evm am65xx_idk j721e_sim j721e_evm j7200_evm am64x_evm
 drvi2c_BOARDLISTLIM    = am65xx_evm am65xx_idk am64x_evm
-drvi2c_SOCLIST         = am574x am572x am571x tda2xx tda2px tda2ex tda3xx dra78x dra72x dra75x k2h k2k k2l k2e k2g c6678 c6657 am437x am335x omapl137 omapl138 am65xx j721e j7200 am64x tpr12 awr294x
+drvi2c_SOCLIST         = am574x am572x am571x tda2xx tda2px tda2ex tda3xx dra78x dra72x dra75x k2h k2k k2l k2e k2g c6678 c6657 am437x am335x omapl137 omapl138 am65xx j721e j7200 j721s2 am64x tpr12 awr294x
 drvi2c_SOCLISTLIM      = am574x am572x am571x tda2xx tda2px tda2ex tda3xx dra78x dra72x dra75x k2h k2k k2l k2e k2g c6678 c6657 am437x am335x omapl137 omapl138 am65xx am64x
 drvi2c_tda2xx_CORELIST = ipu1_0
 drvi2c_tda2px_CORELIST = ipu1_0
@@ -93,6 +93,7 @@ drvi2c_am437x_CORELIST = a9host pru_0 pru_1
 drvi2c_am335x_CORELIST = a8host pru_0 pru_1
 drvi2c_am65xx_CORELIST   = mpu1_0 mcu1_0 mcu1_1
 drvi2c_j721e_CORELIST   = $(DEFAULT_j721e_CORELIST)
+drvi2c_j721s2_CORELIST  = mpu1_0 mcu1_0 mcu2_0 mcu3_0
 drvi2c_j721e_CORELISTARM = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1
 drvi2c_j7200_CORELIST  = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1
 drvi2c_am64x_CORELIST  = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 m4f_0
