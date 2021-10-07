5a514fb89148ff87b331a1f3be9c6ccecf7b0d2b
Don Dominic <a0486429@ti.com>
2021-10-08 00:11:27 +0530

    [HACK] J721S2: Workaround for SBL appimage gen issue
    - SBL libraries are not yet enabled for j721s2,
    - Hence 'ti/boot' directory is not packaged.
    - Due to this sbl_appimagerprc fails in package build
    
    - As a temp hack/workaround the failure, until sbl libs are enabled
      add 'ti/boot' to PACKAGE_SRC_COMMON from board makefile
    
    - HACK commit.
    - This should be reverted when SBL libs are enabled
    
    Signed-off-by: Don Dominic <a0486429@ti.com>

--- packages/ti/board/src/j721s2_evm/src_files_j721s2_evm.mk
+++ packages/ti/board/src/j721s2_evm/src_files_j721s2_evm.mk
@@ -7,3 +7,7 @@ SRCS_COMMON += board_init.c board_lld_init.c board_clock.c board_mmr.c board_pll
 SRCS_COMMON += board_ddr.c board_info.c board_ethernet_config.c board_utils.c board_control.c board_i2c_io_exp.c
 SRCS_COMMON += board_pinmux.c
 PACKAGE_SRCS_COMMON = src/j721s2_evm/src_files_j721s2_evm.mk
+
+# Temp adding the following to package 'ti/boot' directory, until sbl libs are enabled
+# Else there is build error for SBL appimage gen due to missing 'boot' directory
+PACKAGE_SRCS_COMMON = ../boot
