950a926a6076472d8ed9a609cf93378c375ea9d9
Misael Lopez Cruz <misael.lopez@ti.com>
2021-10-19 04:33:23 -0500

    lwip: Fix lwip/lwip-contrib build issue
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

--- packages/ti/transport/lwip/lwip_component.mk
+++ packages/ti/transport/lwip/lwip_component.mk
@@ -76,7 +76,7 @@ drvlwip_BOARDLIST       = am65xx_evm am65xx_idk j721e_evm j7200_evm j721s2_evm t
 drvlwip_am65xx_CORELIST = mcu1_0
 drvlwip_j721e_CORELIST  = mcu1_0 mcu2_0 mcu2_1
 drvlwip_j7200_CORELIST  = mcu1_0 mcu2_0 mcu2_1
-drvenet_j721s2_CORELIST = mcu1_0 mcu2_0 mcu2_1
+drvlwip_j721s2_CORELIST = mcu1_0 mcu2_0 mcu2_1
 drvlwip_tpr12_CORELIST  = mcu1_0 mcu1_1
 drvlwip_awr294x_CORELIST  = mcu1_0 mcu1_1
 
