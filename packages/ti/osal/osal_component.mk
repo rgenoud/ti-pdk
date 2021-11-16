thom24/pdk

[TI Clang Migration][Phase 5]
[TI Clang Migration][Build]Porting + Build Fix

- Switched from '-O3' to '-Oz' optimization level to reduce code size
- Some applications on mcu1_0 were not able to fit into OCMCRAM due to increase in code size because of '-O3'
- Added option to chose between '-O3' and '-Oz'

Signed-off-by: Vivek Dhande <a0132295@ti.com>

[TI Clang Migration][IPC]Porting + Build Fix

- Re-enabled freeRTOS for TIRTOS as this is supported on other cores
- Fixed IPC multi-core app build

Signed-off-by: Vivek Dhande <a0132295@ti.com>

[TI Clang Migration][Fixed Review Comments][IPC]

Signed-off-by: Vivek Dhande <a0132295@ti.com>

[TI Clang Migration][SafeRTOS]Disabled build for safeRTOS

- This is not ported yet by WHIS team

Signed-off-by: Vivek Dhande <a0132295@ti.com>

[TI Clang Migration][Compile Warnings Fixed]Removed '.asmfunc' and '.endasmfunc' to fix compiler warnings

Signed-off-by: Vivek Dhande <a0132295@ti.com>

[TI Clang Migration][Link Warnings Fixed]warning: entry-point symbol other than "_c_int00" specified:

Signed-off-by: Vivek Dhande <a0132295@ti.com>

[TI Clang Migration][Sci-client]Fixed issue with sci-server testApp

- Fixed miss-aligned caused due to misplaced sections 'OCMC_RAM_X509_HEADER1' and 'OCMC_RAM_X509_HEADER2'

Signed-off-by: Vivek Dhande <a0132295@ti.com>

[TI Clang Migration] Re-enable lwIP in makefile

Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

[TI Clang Migration][Build options]Updated build option after aligning with Compiler team

- Added '-O1' option for debug build
- Checked in new ccs init and sci-server testapp bins

Signed-off-by: Vivek Dhande <a0132295@ti.com>

[TI Clang Migration][Build Fix]Disabled Sci-server testapp for debug build to unblock Jenkins

Signed-off-by: Vivek Dhande <a0132295@ti.com>

[TI Clang Migration][Build Fix]Fixed warning while compiling assembly file

- Enabled sci-server testapp for both debug and release mode
- Updated sci-server testapp linker command file

Signed-off-by: Vi

packages/ti/osal/osal_component.mk
@@ -118,7 +118,7 @@ libosal_freertos_j721s2_CORELIST = mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1
 ############################
 osal_LIB_LIST = osal_nonos osal_nonos_indp
 ifeq ($(BUILD_OS_TYPE),tirtos)
-#osal_LIB_LIST += osal_tirtos osal_tirtos_indp
+osal_LIB_LIST += osal_tirtos osal_tirtos_indp
 endif
 osal_LIB_LIST += osal_freertos
 ifeq ($(SOC),$(filter $(SOC), $(libosal_safertos_SOCLIST)))
@@ -257,7 +257,7 @@ export osal_tirtos_PKG_LIST
 osal_tirtos_INCLUDE = $(osal_tirtos_PATH)
 osal_tirtos_SOCLIST = $(libosal_SOCLIST)
 export osal_tirtos_SOCLIST
-osal_tirtos_$(SOC)_CORELIST = $(libosal_$(SOC)_CORELIST)
+osal_tirtos_$(SOC)_CORELIST = $(filter $(DEFAULT_$(SOC)_CORELIST_tirtos), $(libosal_$(SOC)_CORELIST))
 export osal_tirtos_$(SOC)_CORELIST
 
 # OSAL TIRTOS DEVICE INDEPENDENT
@@ -284,7 +284,7 @@ export osal_tirtos_indp_PKG_LIST
 osal_tirtos_indp_INCLUDE = $(osal_tirtos_indp_PATH)
 osal_tirtos_indp_SOCLIST = 
 export osal_tirtos_indp_SOCLIST
-osal_tirtos_indp_$(SOC)_CORELIST = $(libosal_$(SOC)_CORELIST)
+osal_tirtos_indp_$(SOC)_CORELIST = $(filter $(DEFAULT_$(SOC)_CORELIST_tirtos), $(libosal_$(SOC)_CORELIST))
 export osal_tirtos_indp_$(SOC)_CORELIST
 
 # OSAL FREE RTOS LIB

rgenoud/ti-pdk

packages/ti/osal/osal_component.mk
@@ -135,7 +135,7 @@ libosal_safertos_awr294x_CORELIST = c66xdsp_1
 ############################
 osal_LIB_LIST = osal_nonos osal_nonos_indp
 ifeq ($(BUILD_OS_TYPE),tirtos)
-#osal_LIB_LIST += osal_tirtos osal_tirtos_indp
+osal_LIB_LIST += osal_tirtos osal_tirtos_indp
 endif
 osal_LIB_LIST += osal_freertos
 ifeq ($(SOC),$(filter $(SOC), $(libosal_safertos_SOCLIST)))
@@ -274,7 +274,7 @@ export osal_tirtos_PKG_LIST
 osal_tirtos_INCLUDE = $(osal_tirtos_PATH)
 osal_tirtos_SOCLIST = $(libosal_tirtos_SOCLIST)
 export osal_tirtos_SOCLIST
-osal_tirtos_$(SOC)_CORELIST = $(libosal_tirtos_$(SOC)_CORELIST)
+osal_tirtos_$(SOC)_CORELIST = $(filter $(libosal_tirtos_$(SOC)_CORELIST), $(libosal_$(SOC)_CORELIST))
 export osal_tirtos_$(SOC)_CORELIST
 
 # OSAL TIRTOS DEVICE INDEPENDENT
@@ -301,7 +301,7 @@ export osal_tirtos_indp_PKG_LIST
 osal_tirtos_indp_INCLUDE = $(osal_tirtos_indp_PATH)
 osal_tirtos_indp_SOCLIST = 
 export osal_tirtos_indp_SOCLIST
-osal_tirtos_indp_$(SOC)_CORELIST = $(libosal_tirtos_$(SOC)_CORELIST)
+osal_tirtos_indp_$(SOC)_CORELIST = $(filter $(libosal_tirtos_$(SOC)_CORELIST), $(libosal_$(SOC)_CORELIST))
 export osal_tirtos_indp_$(SOC)_CORELIST
 
 # OSAL FREE RTOS LIB