3dc4109776a3076e9f31401c3b8dc955774d5fbb
Sebin Francis <sebin.francis@ti.com>
2021-11-19 00:27:36 +0530

    am62x: Add support for DM r5 self reset for am62x
     - Make the entry point of the DM firmware self_reset_start
     - Update the load and run address of sections placed in TCM memory
     - Add self_reset library for am62x
    
    Fixes: SYSFW-4831
    
    Signed-off-by: Sebin Francis <sebin.francis@ti.com>

--- packages/ti/build/makerules/component.mk
+++ packages/ti/build/makerules/component.mk
@@ -1379,7 +1379,7 @@ endif
 ifeq ($(SOC),$(filter $(SOC), am62x))
   PDK_COMMON_COMP = csl board uart
   ifeq ($(CORE),mcu1_0)
-    PDK_COMMON_COMP +=  sciclient_direct rm_pm_hal dm_stub
+    PDK_COMMON_COMP +=  sciclient_direct rm_pm_hal dm_stub self_reset
   else
     PDK_COMMON_COMP += sciclient
   endif
