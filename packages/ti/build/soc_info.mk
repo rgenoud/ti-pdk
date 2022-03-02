faf10a57141baa0400e7af20ee93642910e16660
Sebin Francis <sebin.francis@ti.com>
2022-03-02 12:20:07 +0530

    am62x: Remove support for m4 core
    Since m4 core is no longer supported in PDK remove M4 core from the list
    of core supported for am62x.
    
    Signed-off-by: Sebin Francis <sebin.francis@ti.com>

--- packages/ti/build/soc_info.mk
+++ packages/ti/build/soc_info.mk
@@ -10,4 +10,4 @@ CORE_LIST_j721e    = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1 c66xdsp_1 
 CORE_LIST_j7200    = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mpu1_1
 CORE_LIST_j721s2   = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1 c7x_1 c7x_2 mpu1_1
 CORE_LIST_j784s4   = mpu1_0 mcu1_0 mcu1_1 mcu2_0 mcu2_1 mcu3_0 mcu3_1 mcu4_0 mcu4_1 c7x_1 c7x_2 c7x_3 c7x_4 mpu1_1 mpu1_2 mpu1_3 mpu2_0 mpu2_1 mpu2_2 mpu2_3
-CORE_LIST_am62x    = mcu1_0 m4f_0
+CORE_LIST_am62x    = mcu1_0
