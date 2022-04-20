a8750b232aba46d81df122c2719bd9b8216dae8b
sebin francis <sebin.francis@ti.com>
2022-04-20 15:16:07 +0530

    am62a: Add board support for am62a
    board init is used in mutiple applications. Add basic bord support for
    am62a and add a empty board int function.
    
    Signed-off-by: sebin francis <sebin.francis@ti.com>

---
+++ packages/ti/board/src/am62a_evm/src_files_am62a_evm.mk
@@ -0,0 +1,8 @@
+
+SRCDIR += src/am62a_evm src/am62a_evm/include
+INCDIR += src/am62a_evm src/am62a_evm/include
+
+# Common source files across all platforms and cores
+SRCS_COMMON += board_init.c
+
+PACKAGE_SRCS_COMMON = src/am62a_evm/src_files_am62a_evm.mk
