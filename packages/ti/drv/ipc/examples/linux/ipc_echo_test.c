512f741a616c4ce20246ad6f67b4d5208d94bdfa
Devarsh Thakkar <devarsht@ti.com>
2022-06-23 17:37:00 +0530

    ipc: examples: Add support for AM62x
    - Enable IPC echo test example for AM62x SOC
    - Add linker scripts base taking reference from
      J721E.
    - Add compilation support for AM62x IPC
      for A53 (linux) <-> DM R5 (MCU1_0)
    - Add R5 MPU config file with VRING address
      as defined by linux devicetree.
    - Remove M4F IPC stub as not fully implemented.
    
    Signed-off-by: Devarsh Thakkar <devarsht@ti.com>

--- packages/ti/drv/ipc/examples/linux/ipc_echo_test.c
+++ packages/ti/drv/ipc/examples/linux/ipc_echo_test.c
@@ -142,7 +142,7 @@ uint32_t remoteProc[] =
 #elif defined (SOC_AM64X)
     IPC_MCU1_0, IPC_MCU1_1, IPC_MCU2_0, IPC_MCU2_1, IPC_M4F_0
 #elif defined (SOC_AM62X)
-    IPC_M4F_0
+    IPC_MCU1_0
 #elif defined (SOC_J721S2)
     IPC_MCU1_0, IPC_MCU1_1, IPC_MCU2_0, IPC_MCU2_1, IPC_MCU3_0, IPC_MCU3_1, IPC_C7X_1, IPC_C7X_2
 #elif defined (SOC_J784S4)
@@ -163,6 +163,8 @@ uint32_t remoteProc[] =
     IPC_MPU1_0, IPC_MCU1_1, IPC_MCU2_0, IPC_MCU2_1
 #elif defined (SOC_AM64X)
     IPC_MPU1_0, IPC_MCU1_1, IPC_MCU2_0, IPC_MCU2_1, IPC_M4F_0
+#elif defined (SOC_AM62X)
+    IPC_MPU1_0
 #elif defined (SOC_AM62A)
     IPC_MPU1_0, IPC_C7X_1
 #elif defined (SOC_J721S2)
