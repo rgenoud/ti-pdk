484f21b353215a8eb1a0ef5ec00ec366d748cb31
Stephen Molfetta <sjmolfetta@ti.com>
2022-08-17 13:54:27 -0500

    sbl: Sequence boot for the HSM M4 processor
    Update the SBL slave core loading to request TIFS to release the HSM
    M4 core from reset.
    
    Update loading sequence to skip the PM SET_DEVICE call in case of an
    invalid ID.
    
    Fixes: PDK-12140
    
    Signed-off-by: Stephen Molfetta <sjmolfetta@ti.com>

--- packages/ti/boot/sbl/soc/k3/sbl_slave_core_boot.c
+++ packages/ti/boot/sbl/soc/k3/sbl_slave_core_boot.c
@@ -788,13 +788,16 @@ void SBL_SlaveCoreBoot(cpu_core_id_t core_id, uint32_t freqHz, sblEntryPoint_t *
             SblErrLoop(__FILE__, __LINE__);
         }
 
-        SBL_log(SBL_LOG_MAX, "Sciclient_pmSetModuleClkFreq, DevId 0x%x @ %dHz... \n", sblSlaveCoreInfoPtr->tisci_dev_id, sblSlaveCoreInfoPtr->slave_clk_freq_hz);
-        SBL_ADD_PROFILE_POINT;
-        Sciclient_pmSetModuleClkFreq(sblSlaveCoreInfoPtr->tisci_dev_id,
-                                     sblSlaveCoreInfoPtr->tisci_clk_id,
-                                     sblSlaveCoreInfoPtr->slave_clk_freq_hz,
-                                     TISCI_MSG_FLAG_AOP,
-                                     SCICLIENT_SERVICE_WAIT_FOREVER);
+        if (sblSlaveCoreInfoPtr->tisci_dev_id != SBL_INVALID_ID)
+        {
+            SBL_log(SBL_LOG_MAX, "Sciclient_pmSetModuleClkFreq, DevId 0x%x @ %dHz... \n", sblSlaveCoreInfoPtr->tisci_dev_id, sblSlaveCoreInfoPtr->slave_clk_freq_hz);
+            SBL_ADD_PROFILE_POINT;
+            Sciclient_pmSetModuleClkFreq(sblSlaveCoreInfoPtr->tisci_dev_id,
+                                         sblSlaveCoreInfoPtr->tisci_clk_id,
+                                         sblSlaveCoreInfoPtr->slave_clk_freq_hz,
+                                         TISCI_MSG_FLAG_AOP,
+                                         SCICLIENT_SERVICE_WAIT_FOREVER);
+        }
         SBL_ADD_PROFILE_POINT;
     }
     else
@@ -988,6 +991,24 @@ void SBL_SlaveCoreBoot(cpu_core_id_t core_id, uint32_t freqHz, sblEntryPoint_t *
             }
 
             SBL_ADD_PROFILE_POINT;
+            break;
+       case HSM_CPU_ID:
+            /* Release core */
+            if (status == CSL_PASS) {
+                status = Sciclient_procBootSetSequenceCtrl(sblSlaveCoreInfoPtr->tisci_proc_id,
+                        0,
+                        TISCI_MSG_VAL_PROC_BOOT_CTRL_FLAG_HSM_M4_RESET,
+                        TISCI_MSG_FLAG_AOP,
+                        SCICLIENT_SERVICE_WAIT_FOREVER);
+                if (status == CSL_PASS)
+                {
+                    if (requestCoresFlag == SBL_REQUEST_CORE)
+                    {
+                        SBL_ReleaseCore(core_id, TISCI_MSG_FLAG_AOP);
+                    }
+                }
+
+            }
             break;
         default:
             SBL_log(SBL_LOG_MAX, "Sciclient_pmSetModuleState Off, DevId 0x%x... \n", sblSlaveCoreInfoPtr->tisci_dev_id);
