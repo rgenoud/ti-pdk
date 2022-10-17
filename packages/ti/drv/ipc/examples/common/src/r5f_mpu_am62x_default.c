7aa8cdedabd7ed4e548c899fa961429212617b63
Devarsh Thakkar <devarsht@ti.com>
2022-10-17 10:28:26 +0530

    am62x: Move resource table and trace buffer by 1Mb
    Move resource table and trace buffer inside by 1Mb,
    to provide compatibility with old linux devicetree
    which preserved address range starting from
    0x9db00000 for DM.
    
    Due to incompatibility with old linux devicetree
    DM was crashing as old kernel was modifying
    the new range 0x9da00000 where resource table
    in new DM was present.
    
    Signed-off-by: Devarsh Thakkar <devarsht@ti.com>

--- packages/ti/drv/ipc/examples/common/src/r5f_mpu_am62x_default.c
+++ packages/ti/drv/ipc/examples/common/src/r5f_mpu_am62x_default.c
@@ -155,7 +155,7 @@ const CSL_ArmR5MpuRegionCfg  gCslR5MpuCfg[CSL_ARM_R5F_MPU_REGIONS_MAX] =
         .regionId         = 7U,
         .enable           = 1U,
 #if defined(BUILD_MCU1_0)
-        .baseAddr         = 0x9d900000,
+        .baseAddr         = 0x9da00000,
 #elif defined(BUILD_MCU1_1)
         .baseAddr         = 0xA1000000,
 #elif defined(BUILD_MCU2_0)
@@ -177,7 +177,7 @@ const CSL_ArmR5MpuRegionCfg  gCslR5MpuCfg[CSL_ARM_R5F_MPU_REGIONS_MAX] =
         .regionId         = 8U,
         .enable           = 1U,
 #if defined(BUILD_MCU1_0)
-        .baseAddr         = 0x9dA00000,
+        .baseAddr         = 0x9db00000,
 #elif defined(BUILD_MCU1_1)
         .baseAddr         = 0xA1000000,
 #elif defined(BUILD_MCU2_0)
