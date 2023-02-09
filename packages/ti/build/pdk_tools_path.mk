8b95cd3de00e9012e210ddd7c4e01fd1e179558e
Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>
2023-02-09 10:51:19 +0530

    [AM62A] Updated the c7x compiler to v3.1.0B2  - Compiler version updated only for AM62A
    Signed-off-by: Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>

--- packages/ti/build/pdk_tools_path.mk
+++ packages/ti/build/pdk_tools_path.mk
@@ -19,7 +19,7 @@ endif
   MMWAVE_DFP_VERSION=02_04_00_01
 
 ifeq ($(BOARD),$(filter $(BOARD), am62a_evm am62a_hostemu am62a_loki))
-  CGT_C7X_VERSION=3.0.0.STS
+  CGT_C7X_VERSION=3.1.0B2
 else
   CGT_C7X_VERSION=2.1.1.LTS
 endif
