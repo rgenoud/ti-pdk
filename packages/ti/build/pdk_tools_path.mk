924684221dbf978ac9c887f2f06a18e4be112e1c
Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>
2023-02-15 22:36:45 +0530

    [AM62A] C7000 compiler version is changed to 3.1.0.LTS
     -  C7000 compiler version is changed to 3.1.0.LTS for AM62A alone.
    
    Signed-off-by: Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>

--- packages/ti/build/pdk_tools_path.mk
+++ packages/ti/build/pdk_tools_path.mk
@@ -19,7 +19,7 @@ endif
   MMWAVE_DFP_VERSION=02_04_00_01
 
 ifeq ($(BOARD),$(filter $(BOARD), am62a_evm am62a_hostemu am62a_loki))
-  CGT_C7X_VERSION=3.1.0B2
+  CGT_C7X_VERSION=3.1.0.LTS
 else
   CGT_C7X_VERSION=2.1.1.LTS
 endif
