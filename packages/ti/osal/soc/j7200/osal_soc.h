f54cb97aa671edf22b8513242a44a3c7bd4b14dd
Meghana Malladi <m-malladi@ti.com>
2024-02-02 19:07:53 +0530

    Osal: Increase max supported mutex number per soc to 32U.
    While running ETHFW with TSN, it demands around 20-22 mutexes;
    exhausting the existing supported max mutex number, hence increasing
    it to 32U in J721E, J7200, J784S4 and J721S2
    
    Signed-off-by: Meghana Malladi <m-malladi@ti.com>

--- packages/ti/osal/soc/j7200/osal_soc.h
+++ packages/ti/osal/soc/j7200/osal_soc.h
@@ -122,7 +122,7 @@ extern "C" {
 #define OSAL_FREERTOS_MAX_TIMERP_PER_SOC     (TimerP_numTimerDevices)
 #define OSAL_FREERTOS_MAX_TASKP_PER_SOC      ((uint32_t) 40U)
 #define OSAL_FREERTOS_MAX_CLOCKP_PER_SOC     ((uint32_t) 20U)
-#define OSAL_FREERTOS_MAX_MUTEXP_PER_SOC     ((uint32_t) 20U)
+#define OSAL_FREERTOS_MAX_MUTEXP_PER_SOC     ((uint32_t) 32U)
 #define OSAL_FREERTOS_MAX_MAILBOXP_PER_SOC   ((uint32_t) 20U)
 #define OSAL_FREERTOS_MAX_QUEUEP_PER_SOC     ((uint32_t) 20U)
 #define OSAL_FREERTOS_MAX_HEAPP_PER_SOC      ((uint32_t) 20U)
