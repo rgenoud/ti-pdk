d41fbc8c3d5ed7021ae01c96e6a4210d5ea7cf93
Pradeepa <x1074845@ti.com>
2024-05-28 16:31:55 +0530

    [UDMA]: Misra issue fix.
    1 Misra issue fixed with checker code VA_UNUSED.INIT.
    
    Signed-off-by: Pradeepa Chandrashekar x1074845@ti.com

--- packages/ti/drv/udma/src/proxy/V0/src/csl_proxy.c
+++ packages/ti/drv/udma/src/proxy/V0/src/csl_proxy.c
@@ -147,11 +147,19 @@ uint32_t CSL_proxyGetMaxMsgSize( const CSL_ProxyCfg *pProxyCfg, uint32_t targetN
 
 uintptr_t CSL_proxyGetDataAddr( const CSL_ProxyCfg *pProxyCfg, uint32_t targetNum, uint32_t threadNum, uint32_t numBytes )
 {
-    uintptr_t dataAddr;
+    uintptr_t dataAddr = NULL;
     CSL_ProxyTargetParams *pProxyTargetParams = &((pProxyCfg->pProxyTargetParams)[targetNum]);
 
-    dataAddr =  (uintptr_t)pProxyTargetParams->pTargetRegs->PROXY[threadNum].DATA;
-    dataAddr += (uintptr_t)CSL_PROXY_TARGET_MAX_MSG_SIZE - (uintptr_t)numBytes;
+    if(pProxyTargetParams != NULL)
+    {
+    	dataAddr =  (uintptr_t)pProxyTargetParams->pTargetRegs->PROXY[threadNum].DATA;
+    	dataAddr += (uintptr_t)CSL_PROXY_TARGET_MAX_MSG_SIZE - (uintptr_t)numBytes;
+    }
+    else
+    {
+    	//do nothing
+    }
+
     return dataAddr;
 }
 
